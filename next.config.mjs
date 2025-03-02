42c27ac127dc9d386f9ad83be1205d715482e886
PinsaraPerera <1pawanpinsara@gmail.com>
2025-03-02 18:16:31 +0530

    fixed next configuration to github pages build

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,22 @@
 import nextra from 'nextra'
- 
+
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx'
-})
- 
-export default withNextra()
- 
-// If you have other Next.js configurations, you can pass them as the parameter:
-// export default withNextra({ /* other next.js config */ })
+  themeConfig: './theme.config.jsx',
+});
+
+const isProd = process.env.NODE_ENV === 'production';
+
+export default withNextra({
+  // Ensure the project builds for static export
+  output: 'export',
+
+  // Support for GitHub Pages
+  basePath: isProd ? '/batch5' : '',
+  assetPrefix: isProd ? '/batch5' : '',
+
+  // Required if you're using next/image
+  images: {
+    unoptimized: true,
+  },
+});
