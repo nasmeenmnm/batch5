439d8665454b16ee289d8920d91c1e25518f5349
PinsaraPerera <1pawanpinsara@gmail.com>
2025-03-07 21:14:07 +0530

    file changes to pages/Meet_our_team

---
+++ pages/Meet_our_team/_meta.js
@@ -0,0 +1,16 @@
+
+const meta = {
+    batch: "About us",
+    '---': {
+        type: "separator",
+    },
+    Pawan_Pinsara: "Pawan Pinsara",
+    Amindu_Bhashana: "Amindu Bhashana",
+    Viduruwan: "Y.P. Viduruwan",
+    Mihitha_Wanniarachchi: "Mihitha Wanniarachchi",
+    Tharusha: "Tharusha Kavindi",
+    Apeksha:"Apeksha Pathirage",
+    Dilshana_Ranawake: "Dilshana Ranawake",
+}
+
+export default meta;
