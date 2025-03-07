4b96c04e7c0bc0f36017fda43744c90cf12e2d5e
PinsaraPerera <1pawanpinsara@gmail.com>
2025-03-07 23:32:04 +0530

    add two others

--- pages/Meet_our_team/_meta.js
+++ pages/Meet_our_team/_meta.js
@@ -6,11 +6,13 @@ const meta = {
     },
     Pawan_Pinsara: "Pawan Pinsara",
     Amindu_Bhashana: "Amindu Bhashana",
+    Tharindu_Damruwan: "Tharindu Damruwan",
     Viduruwan: "Y.P. Viduruwan",
     Mihitha_Wanniarachchi: "Mihitha Wanniarachchi",
     Tharusha: "Tharusha Kavindi",
     Apeksha:"Apeksha Pathirage",
     Dilshana_Ranawake: "Dilshana Ranawake",
+    Piyumi_Weerarathna: "Piyumi Weerarathna",
 }
 
 export default meta;
