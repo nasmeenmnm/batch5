15a804398f9f6dcccd6e6c8ea86a1f8c0751392d
PinsaraPerera <1pawanpinsara@gmail.com>
2025-03-07 21:13:41 +0530

    _meta structure changed

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,14 +1,14 @@
 const meta = {
     index: "Introduction",
-    about: "About Us",
-    component: "Meet the Team",
-    Pawan_Pinsara: "Pawan Pinsara",
-    Amindu_Bhashana: "Amindu Bhashana",
-    Viduruwan: "Y.P. Viduruwan",
-    Mihitha_Wanniarachchi: "Mihitha Wanniarachchi",
-    Tharusha: "Tharusha Kavindi",
-    Apeksha:"Apeksha Pathirage",
-    Dilshana_Ranawake: "Dilshana Ranawake",
+    team_link: {
+        title: "Meet our team",
+        href: "/Meet_our_team/batch",
+    },
+    Meet_our_team: {
+        title:"Meet our team",
+        type:"page",
+    },
+    component: "Example Router",
 }
 
 export default meta;
