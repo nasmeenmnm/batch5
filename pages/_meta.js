7b9fd12c571b19760c7fb12fc85d896bbb92fb48
Tharindu209 <113744956+Tharindu209@users.noreply.github.com>
2025-03-07 16:52:01 +0000

    add the index

--- pages/_meta.js
+++ pages/_meta.js
@@ -5,6 +5,7 @@ const meta = {
     Pawan_Pinsara: "Pawan Pinsara",
     Amindu_Bhashana: "Amindu Bhashana",
     Viduruwan: "Y.P. Viduruwan",
+    Tharindu_Damruwan: "Tharindu Damruwan",
     Mihitha_Wanniarachchi: "Mihitha Wanniarachchi",
     Tharusha: "Tharusha Kavindi",
     Apeksha:"Apeksha Pathirage",
